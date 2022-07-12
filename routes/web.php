8b53bfef1b365ee26821db4ec01252b03642a9a0
Imam Ramadhan <imamrmdn99@gmail.com>
2022-07-12 08:21:22 +0700

    article page

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-
+use App\Http\Controllers\ArticleController;
+ 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('\sk', function ()
-{
-    return view('extra');
-});
+Route::get('/article', [ArticleController::class, "index"]);
+Route::get('/article/1', [ArticleController::class, "show"]);
